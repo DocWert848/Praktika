34d6c5eba36edc1c88224560a300925fdcca4609
DocWert848 <ruskhay1703@yandex.ru>
2025-12-11 11:53:20 +0300

    Update Задание 9.py

--- Задание 9.py
+++ Задание 9.py
@@ -14,13 +14,16 @@ while not proverka:
             notebook[pynkt] = cell
             print(notebook)
         elif deustvue == 2:
-            for pynkt, cell in notebook.items():
-                print(f"Заметка: {pynkt}: {cell}")
-            pynkt = input("введите пункт заметки:")
-            if pynkt in notebook:
-                print(f"заметка {pynkt}: {notebook[pynkt]}")
-            elif pynkt not in notebook:
-                print(f"пункта {pynkt} нет")
+            if notebook == {}:
+                print("Заметок нет")
+            else:
+                for pynkt, cell in notebook.items():
+                    print(f"Заметка: {pynkt}: {cell}")
+                pynkt = input("введите пункт заметки:")
+                if pynkt in notebook:
+                    print(f"заметка {pynkt}: {notebook[pynkt]}")
+                elif pynkt not in notebook:
+                    print(f"пункта {pynkt} нет")
         elif deustvue == 3:
             for pynkt, cell in notebook.items():
                 print(f"Заметка: {pynkt}: {cell}")
