996467bf20f763c91a474f3f0dc9637a36f12e25
DocWert848 <ruskhay1703@yandex.ru>
2025-12-11 11:20:25 +0300

    Update Задание 9.py

--- Задание 9.py
+++ Задание 9.py
@@ -1,14 +1,26 @@
 notebook = {}
 proverka = False
 while not proverka:
+    print(
+        "[1]- сделать заметку. [2] - посмотреть заметки. [3] -удалить заметку. [4] -выход."
+    )
     deustvue = int(input("введите действие которое хотите выполнить:"))
-    print ("[1]- сделать заметку. [2] - посмотреть заметки. [3] -удалить заметку. [4] -выход.")
     if deustvue > 0 and deustvue < 5:
-        proverka = True
+        proverka = False
+        if deustvue == 1:
+            pynkt = input("введите пункт цели:")
+            cell = input("введите цель:")
+            notebook[pynkt] = cell
+            print(notebook)
+        elif deustvue == 2:
+            print("ваши заметки:")
+            print(notebook)
+        elif deustvue == 3:
+            delite = input("введите пункт который хотите удалить:")
+            notebook.pop(delite)
+            print(notebook)
+        elif deustvue == 4:
+            print("вы вышли из заметок")
+            proverka = True
     else:
         proverka = False
-if deustvue == 1:
-    pynkt = (input("введите пункт цели:"))
-    cell = (input("введите цель:"))
-    notebook[pynkt] = cell
-    print (notebook)
