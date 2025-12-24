924b587494a9999f4666ab07f8093851ff839d60
DocWert848 <ruskhay1703@yandex.ru>
2025-12-24 15:19:20 +0300

    Update Задание 10.py

--- Задание 10.py
+++ Задание 10.py
@@ -56,3 +56,77 @@ def double(s):
 
 
 double("stroka")
+
+#Задание 5
+def Constructor(details, people, cars, trees):
+    required_details = 72
+    required_people = 4
+    required_cars = 2
+    required_trees = 7
+    max_sets = min(
+        details // required_details,
+        people // required_people,
+        cars // required_cars,
+        trees // required_trees,
+    )
+    print(max_sets)
+
+
+print(">>> Constructor(144, 8, 4, 14)")
+Constructor(144, 8, 4, 14)
+print(">>> Constructor(10000, 16, 6, 2)")
+Constructor(10000, 16, 6, 2)
+
+
+#Задание 6
+def Constructor(details, people, cars, trees):
+    required_details = 72
+    required_people = 4
+    required_cars = 2
+    required_trees = 7
+    max_sets = min(
+        details // required_details,
+        people // required_people,
+        cars // required_cars,
+        trees // required_trees,
+    )
+    print(max_sets)
+
+
+print(">>> Constructor(144, 8, 4, 14)")
+Constructor(144, 8, 4, 14)
+print(">>> Constructor(10000, 16, 6, 2)")
+Constructor(10000, 16, 6, 2)
+
+
+#Задание 7
+def custom_filter(data_list):
+    total_sum = 0
+    for item in data_list:
+        if isinstance(item, int) and not isinstance(item, bool):
+            if item % 7 == 0:
+                total_sum += item
+    print(f">>> сумма: {total_sum}")
+    print(total_sum <= 83)
+
+
+print(">>> print(custom_filter([7, 10.5, 'txt', 14, 2, 56]))")
+custom_filter([7, 10.5, "txt", 14, 2, 56])
+print(">>> print(custom_filter([70, 21]))")
+custom_filter([70, 21])
+
+
+#Задание 8
+def square(x, y):
+    top_border = "_" * (len(str(x)) + len(str(y)) + 3)
+    print(top_border)
+    middle = f"| {x} {y} |"
+    print(middle)
+    bottom_border = "¯" * (len(str(x)) + len(str(y)) + 3)
+    print(bottom_border)
+
+
+print(">>> square(2, 3)")
+square(2, 3)
+print(">>> square(100, 5000)")
+square(100, 5000)
